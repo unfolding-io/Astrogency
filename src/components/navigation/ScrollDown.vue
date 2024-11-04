824da2b650253580cb1d0106c09a55206d2b6d31
preetamslot <preetam.slot@gmail.com>
2024-11-04 14:52:02 +0100

    fix icon buttons

--- src/components/navigation/ScrollDown.vue
+++ src/components/navigation/ScrollDown.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="pointer-events none sticky inset-x-0 bottom-4 -mb-5 -mt-5 flex h-10 items-center justify-end z-40">
-    <button aria-label="scroll down or up"
-      class="scroll-down btn btn-icon pointer-events-auto m-4 h-10 w-10 text-2xl"
-      :class="surface || 'surface-secondary'" @click="scrollUpOrDown">
-      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
-        class="pointer-events-none transition-all duration-200" viewBox="0 0 24 24">
-        <g transform="rotate(-90 12 12)">
-          <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-            <path d="M19 12H5.5"></path>
-            <path d="M5 12L10 17M5 12L10 7"></path>
-          </g>
-        </g>
+  <div
+    class="pointer-events none sticky inset-x-0 bottom-4 z-40 -mb-5 -mt-5 flex h-8 items-center justify-end"
+  >
+    <button
+      aria-label="scroll down or up"
+      class="scroll-down btn-icon pointer-events-auto m-4 text-xl"
+      :class="surface || 'surface-secondary'"
+      @click="scrollUpOrDown"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+        class="transition-transform duration-300 ease-in-out"
+      >
+        <path
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 5v14m7-7l-7 7l-7-7"
+        />
       </svg>
     </button>
   </div>
